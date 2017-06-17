40a19d60d413b667318f0e0c18a4d24203441878
Johan Smet <johan.smet@justcode.be>
2017-06-17 17:04:39 +0200

    Introduced command line arguments to make incremental training easier

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import numpy as np
 
 import argparse
 import os.path
+
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
@@ -10,6 +11,7 @@ import cv2
 
 from keras.layers import Flatten, Dense, Conv2D, Lambda, AveragePooling2D, Dropout
 from keras.models import Sequential
+from keras.models import load_model
 from keras import callbacks
 
 STEERING_CORRECTION = 0.2
@@ -74,37 +76,47 @@ class DataSet:
 
 
 class NvidiaModel:
-    def __init__(self):
+    def __init__(self, load):
         self.tensorboard = callbacks.TensorBoard(log_dir='./logs', histogram_freq=10, write_graph=True, write_images=True)
+        
+        if load:
+            self.model = load_model('model.h5')
+        else:
+            self.model = Sequential()
+            self.model.add(AveragePooling2D((1,2), input_shape=(160,320,3)))
+            self.model.add(Lambda(lambda x : (x / 255.0) - 0.5))
+            self.model.add(Conv2D(24, 5, 5, subsample=(2,2), activation='relu'))
+            self.model.add(Conv2D(36, 5, 5, subsample=(2,2), activation='relu'))
+            self.model.add(Conv2D(48, 5, 5, subsample=(2,2), activation='relu'))
+            self.model.add(Conv2D(64, 3, 3, activation='relu'))
+            self.model.add(Conv2D(64, 3, 3, activation='relu'))
+            self.model.add(Dropout(0.2))
+            self.model.add(Flatten())
+            self.model.add(Dense(100))
+            self.model.add(Dense(50))
+            self.model.add(Dense(10))
+            self.model.add(Dense(1))
 
-        self.model = Sequential()
-        self.model.add(AveragePooling2D((1,2), input_shape=(160,320,3)))
-        self.model.add(Lambda(lambda x : (x / 255.0) - 0.5))
-        self.model.add(Conv2D(24, 5, 5, subsample=(2,2), activation='relu'))
-        self.model.add(Conv2D(36, 5, 5, subsample=(2,2), activation='relu'))
-        self.model.add(Conv2D(48, 5, 5, subsample=(2,2), activation='relu'))
-        self.model.add(Conv2D(64, 3, 3, activation='relu'))
-        self.model.add(Conv2D(64, 3, 3, activation='relu'))
-        self.model.add(Dropout(0.2))
-        self.model.add(Flatten())
-        self.model.add(Dense(100))
-        self.model.add(Dense(50))
-        self.model.add(Dense(10))
-        self.model.add(Dense(1))
+            self.model.compile(loss='mse', optimizer='adam')
 
-        self.model.compile(loss='mse', optimizer='adam')
-
-    def train(self, dataset):
+    def train(self, dataset, epochs):
         self.model.fit_generator(dataset.generator_train, samples_per_epoch=len(dataset.data_train)*2, 
                                  validation_data=dataset.generator_valid, nb_val_samples=len(dataset.data_validate)*2, 
                                  callbacks=[self.tensorboard],
-                                 nb_epoch=5)
+                                 nb_epoch=epochs)
 
     def save(self):
         self.model.save('model.h5')
 
 if __name__ == "__main__":
-    dataset = DataSet('data')
-    model = NvidiaModel()
-    model.train(dataset)
+    # handle command line arguments
+    parser = argparse.ArgumentParser(description='Driving Model Trainer')
+    parser.add_argument("-l", "--load", help="Load model.h5 and continue training", action="store_true")
+    parser.add_argument("-e", "--epochs", help="Set number of epochs (defaults=3)", type=int)
+    parser.add_argument("datadir", help="Path to training data")
+    args = parser.parse_args()
+
+    dataset = DataSet(args.datadir)
+    model = NvidiaModel(args.load)
+    model.train(dataset, args.epochs if args.epochs != None else 3)
     model.save()
