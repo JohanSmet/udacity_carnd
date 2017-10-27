74973646f5bb3c2f71037ba746480bdd8cacc7c1
Johan Smet <johan@ivy.johan.home>
2017-10-27 18:48:31 +0200

    Implement CalculateRMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -9,10 +9,28 @@ namespace Tools {
 
 VectorXd CalculateRMSE(const vector<VectorXd> &estimations,
                        const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd rmse(4);
+  rmse.fill(0.0);
+
+  // check parameters
+  if (estimations.empty() || estimations.size() != ground_truth.size()) {
+    std::cerr << "CalculateRMSE() : invalid parameters" << std::endl;
+    return rmse;
+  }
+
+  // accumulate squared residuals
+  for (int i=0; i < estimations.size(); ++i) {
+    VectorXd delta = estimations[i] - ground_truth[i];
+    rmse = rmse.array() + (delta.array() * delta.array());
+  }
+
+  // calculate the mean
+  rmse = rmse / estimations.size();
+
+  // calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
 
 } // namespace Tools
